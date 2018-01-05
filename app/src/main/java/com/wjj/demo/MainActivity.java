cdae9cddb1bb83eb6f94dacbbe86fe5ae39a6367
heartbeatings <1540324558@qq.com>
2018-01-05 22:12:22 +0800

    1

--- app/src/main/java/com/wjj/demo/MainActivity.java
+++ app/src/main/java/com/wjj/demo/MainActivity.java
@@ -11,8 +11,22 @@ import android.telephony.SmsManager;
 import com.apkfuns.logutils.LogUtils;
 import com.wjj.demo.BroadcastReceiver.SMSBroadcastReceiver;
 import com.wjj.demo.Model.PhoneMessage;
+import java.util.StringTokenizer;
 
 import java.util.List;
+
+
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +42,8 @@ public class MainActivity extends BaseActivity {
     @BindView(R.id.text)
     TextView text;
 
+    String Code = "1111";
+
     @Override
     int getLayout() {
         return R.layout.activity_main;
@@ -38,7 +54,10 @@ public class MainActivity extends BaseActivity {
         ButterKnife.bind(this);
         intentList = getIntent().getStringArrayListExtra("dataList");
     }
+
     int is = 0;
+    SmsManager smsMgr = SmsManager.getDefault();
+
     @Override
     void logic() {
         //授权
@@ -56,24 +75,52 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onReceived(PhoneMessage phoneMessage) {
                 for (int k = 0; k < intentList.size(); k++) {
+
                     if (phoneMessage.getPhoneNumber().contains(intentList.get(k))) {
-                        is=1;
-                        SmsManager smsMgr1=SmsManager.getDefault();
-                        smsMgr1.sendTextMessage(phoneMessage.getPhoneNumber(),null,"Accept successfully!", null, null);
+                        is = 1;
+
 
                         String msg = phoneMessage.getPhoneNumber() + "\n" + phoneMessage.getMsgTime() + "\n" + phoneMessage.getMsgContent();
                         LogUtils.d("--->发送的对象是 " + msg);
                         text.setText(msg);
-                        if(phoneMessage.getMsgContent().equals("JWUJ ") ){
-                            Vibrator vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+                        StringTokenizer token = new StringTokenizer(phoneMessage.getMsgContent(), "#");
+                        String[] array = new String[3];//定义一个字符串数组
+                        int i = 0;
+                        while (token.hasMoreTokens()) {
+                            array[i] = token.nextToken();//将分割开的子字符串放入数组中
+                            i++;
+                        }
+
+                        if (array[0].equals("BOMB") && array[1].equals(Code)) {
+
+                            smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "引爆成功!", null, null);
+
+                            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
                             long[] pattern = {800, 50, 400, 30};
                             vibrator.vibrate(pattern, 0);
+                        } else if (array[0].equals("CHANGE")) {
+                            if (array[1].equals(Code))
+                                Code = array[2];
+
+                            smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "修改成功!", null, null);
+
+                        }
+                        else if(array[0].equals("LOCATION") && array[1].equals(Code)){
+
+                                    smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "经度为：28°10'"+"\n"+"纬度为：28°10'" , null, null);
+
+                        }
+                        else if(array[1].equals(Code)){
+                            smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "指令错误!", null, null);
+                        }
+                        else{
+                            smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "密码错误!", null, null);
                         }
                     }
                 }
-                if(is == 0){
-                    SmsManager smsMgr=SmsManager.getDefault();
-                    smsMgr.sendTextMessage("18374883754", null, phoneMessage.getPhoneNumber() + "\n" + phoneMessage.getMsgTime() + "\n" + phoneMessage.getMsgContent(), null, null);
+                if (is == 0) {
+
+                    smsMgr.sendTextMessage(intentList.get(0), null, "未授权者请求："+'\n'+phoneMessage.getPhoneNumber() + '\n' + phoneMessage.getMsgTime() + '\n' + phoneMessage.getMsgContent(), null, null);
 
                 }
             }
@@ -89,15 +136,6 @@ public class MainActivity extends BaseActivity {
     @Override
     void onResumeLogic() {
 
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mSMSBroadcastReceiver != null) {
-            this.unregisterReceiver(mSMSBroadcastReceiver);
-            mSMSBroadcastReceiver = null;
-        }
     }
 
 
@@ -120,4 +158,10 @@ public class MainActivity extends BaseActivity {
                                            int[] grantResults) {
         PermissionGen.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
     }
+
+
+
+
 }
+
+
--- app/src/main/java/com/wjj/demo/SettingActivity.java
+++ app/src/main/java/com/wjj/demo/SettingActivity.java
@@ -103,4 +103,6 @@ public class SettingActivity extends BaseActivity {
                 break;
         }
     }
+
 }
+
