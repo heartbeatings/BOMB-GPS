f1ab08b48d54e11aad1b6a09d1ac33dd7f6cfb9a
heartbeatings <1540324558@qq.com>
2018-01-08 08:46:37 +0800

    Final

--- app/src/main/java/com/wjj/demo/MainActivity.java
+++ app/src/main/java/com/wjj/demo/MainActivity.java
@@ -2,10 +2,13 @@ package com.wjj.demo;
 
 
 import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Random;
 import android.os.Vibrator;
 import android.telephony.SmsManager;
 import com.apkfuns.logutils.LogUtils;
@@ -39,6 +42,13 @@ public class MainActivity extends BaseActivity {
     private SMSBroadcastReceiver mSMSBroadcastReceiver;
     private List<String> intentList;
     private Handler handler;
+    private String cmd = "";
+    private String Num = "";
+    private String L1 = "";
+    private String L2 = "";
+
+    private IntentFilter sendFilter;
+
     @BindView(R.id.text)
     TextView text;
 
@@ -51,12 +61,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     void init() {
+
+
         ButterKnife.bind(this);
         intentList = getIntent().getStringArrayListExtra("dataList");
     }
 
     int is = 0;
-    SmsManager smsMgr = SmsManager.getDefault();
 
     @Override
     void logic() {
@@ -65,7 +76,8 @@ public class MainActivity extends BaseActivity {
                 .addRequestCode(100)
                 .permissions(
                         Manifest.permission.RECEIVE_SMS,
-                        Manifest.permission.READ_SMS)
+                        Manifest.permission.READ_SMS,
+                        Manifest.permission.SEND_SMS)
                 .request();
 
         //生成广播处理
@@ -74,9 +86,11 @@ public class MainActivity extends BaseActivity {
         mSMSBroadcastReceiver.setOnReceivedMessageListener(new SMSBroadcastReceiver.MessageListener() {
             @Override
             public void onReceived(PhoneMessage phoneMessage) {
+                Num = intentList.get(0);
+
                 for (int k = 0; k < intentList.size(); k++) {
 
-                    if (phoneMessage.getPhoneNumber().contains(intentList.get(k))) {
+                    if (phoneMessage.getPhoneNumber().equals(intentList.get(k))) {
                         is = 1;
 
 
@@ -92,39 +106,57 @@ public class MainActivity extends BaseActivity {
                         }
 
                         if (array[0].equals("BOMB") && array[1].equals(Code)) {
-
-                            smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "引爆成功!", null, null);
-
+                            cmd = "引爆成功";
+                            // smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "引爆成功!", null, null);
+                          send();
                             Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
                             long[] pattern = {800, 50, 400, 30};
                             vibrator.vibrate(pattern, 0);
-                        } else if (array[0].equals("CHANGE")) {
-                            if (array[1].equals(Code))
-                                Code = array[2];
-
-                            smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "修改成功!", null, null);
+                        } else if (array[0].equals("CHANGE") &&array[1].equals(Code)) {
+                            Code = array[2];
+                            cmd = "修改成功";
+                          send();
+                            //smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "修改成功!", null, null);
 
                         }
-                        else if(array[0].equals("LOCATION") && array[1].equals(Code)){
-
-                                    smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "经度为：28°10'"+"\n"+"纬度为：28°10'" , null, null);
-
+                        else if(array[0].equals("GPS") && array[1].equals(Code)){
+                            calculate();
+                            cmd = "经度为：113°" + L1 + "\'\n" + "纬度为：28°" + L2 + "\'";
+                            //      smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "经度为：28°10'"+"\n"+"纬度为：28°10'" , null, null);
+                            send();
                         }
                         else if(array[1].equals(Code)){
-                            smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "指令错误!", null, null);
+                            cmd = "指令错误!";
+                            send();
+                            //smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "指令错误!", null, null);
                         }
                         else{
-                            smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "密码错误!", null, null);
+                            cmd = "密码错误!";
+                          send();
+                            //smsMgr.sendTextMessage(phoneMessage.getPhoneNumber(), null, "密码错误!", null, null);
                         }
+
+
                     }
                 }
                 if (is == 0) {
-
-                    smsMgr.sendTextMessage(intentList.get(0), null, "未授权者请求："+'\n'+phoneMessage.getPhoneNumber() + '\n' + phoneMessage.getMsgTime() + '\n' + phoneMessage.getMsgContent(), null, null);
-
+                    cmd = "未授权者请求："+'\n'+phoneMessage.getPhoneNumber() + '\n' + phoneMessage.getMsgTime() + '\n' + phoneMessage.getMsgContent();
+                    //smsMgr.sendTextMessage(intentList.get(0), null, "未授权者请求："+'\n'+phoneMessage.getPhoneNumber() + '\n' + phoneMessage.getMsgTime() + '\n' + phoneMessage.getMsgContent(), null, null);
+                    send();
                 }
+                text.setText(cmd);
             }
         });
+
+    }
+   public void send(){
+        String temp = cmd;
+        sendFilter = new IntentFilter();
+        sendFilter.addAction("SENT_SMS_ACTION");
+        final SmsManager smsManager = SmsManager.getDefault();
+        Intent sentIntent = new Intent("SENT_SMS_ACTION");
+        final PendingIntent pi = PendingIntent.getBroadcast(MainActivity.this, 0, sentIntent, 0);
+        smsManager.sendTextMessage(Num, null, temp, pi, null);
     }
 
     @Override
@@ -151,6 +183,19 @@ public class MainActivity extends BaseActivity {
     @PermissionFail(requestCode = 100)
     public void doFailRegisterReceiver() {
         Toast.makeText(this, "Contact permission is not granted", Toast.LENGTH_SHORT).show();
+    }
+
+    public void calculate(){
+        int max=8;
+        int min=2;
+        Random random = new Random();
+
+        int s1 = random.nextInt(max)%(max-min+1) + min + 20;
+        int s2 = random.nextInt(max)%(max-min+1) + min;
+
+        L1 = Integer.toString(s1);
+        L2 = Integer.toString(s2);
+
     }
 
     @Override
